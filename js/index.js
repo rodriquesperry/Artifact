a736e05178af6e0c4c1923d3812688fbaf7dfa8e
Rodriques <rodriques.perry@gmail.com>
2021-01-07 09:35:02 -0600

    Add time graph

--- js/index.js
+++ js/index.js
@@ -87,7 +87,9 @@ $('.projectBtn').tooltip();
 $('.tasks-tab-card').tooltip();
 $('.canvasTitle').tooltip();
 $('project-name').tooltip();
-let myCircle = Circles.create({
+
+
+Circles.create({
   id:                  'circle-thisWeek',
   radius:              50,
   value:               75,
@@ -121,7 +123,45 @@ Circles.create({
   styleText:           true
 });
 
+let ctx = $('#timeChart');
 
+let timeChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+      labels: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+      datasets: [{
+          label: 'hours worked',
+          data: [0, 12, 3, 5, 2, 3, 6],
+          backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+      }]
+  },
+  options: {
+    responsive: true,
+      scales: {
+          yAxes: [{
+              ticks: {
+                  beginAtZero: true
+              }
+          }]
+      }
+  }
+});
 
 
 
